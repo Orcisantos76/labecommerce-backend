0c32248014d572f808f7e0ae016c22e8754f2ae2
Orcisantos76 <orcisantos76@gmail.com>
2023-07-12 14:49:22 -0300

    Referenciando tabelas

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -15,6 +15,15 @@ CREATE TABLE products (
     image_url TEXT NOT NULL
 );
 
+CREATE TABLE purchases(
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    buyer TEXT NOT NULL,
+    total_price REAL NOT NULL,
+    created_at TEXT NOT NULL,
+
+    Foreign Key (buyer) REFERENCES users(id)
+);
+
 INSERT INTO users (id, name, email, password, create_at)
 VALUES
     ('u001','Orci','orci@email.com','orci1234',datetime('now')),
@@ -26,24 +35,65 @@ VALUES
     ('prod003', 'Teclado mecanico', 400.00 , 'Teclado mais preciso', 'https://picsum.photos/seed/teclado/400'  ),
     ('prod004', 'Notebook', 6000.00 , 'Nitro 5', 'https://picsum.photos/seed/notebook/400'  ),
     ('prod005', 'Cadeira gamer', 1500.00,'Cadeira gamer para horas de games com maior conforto', 'https://picsum.photos/seed/cadeiragamer/400');
-    
+
+INSERT INTO purchases (id,buyer,total_price,created_at)
+VALUES
+    ('p001','u001',400,datetime('now')),
+    ('p002','u001',300,datetime('now')),
+    ('p003','u002',350,datetime('now')),
+    ('p004','u002',500,datetime('now'));
+
+--atualizar valor.
+UPDATE purchases
+SET total_price = 999
+WHERE id = 'p004';
+
+
+--Informaçao da tabela
 PRAGMA table_info ('users');
+
 PRAGMA table_info ('products');
 
---busca tabela de users:
+PRAGMA table_info ('purchases');
+
+-- Excluir/apagar tabela
+DROP TABLE products;
+
+--busca mostrar tabela de users:
 SELECT * FROM users;
 
 --busca a tabela de products: 
 SELECT * FROM products;
 
+SELECT * FROM purchases;
+
+SELECT
+    purchases.id AS idPurchase,
+    users.id AS idUser,
+    users.name,
+    users.email,
+    purchases.total_price,
+    purchases.created_at
+FROM purchases
+INNER JOIN users
+ON purchases.buyer = users.id;
+
 --busca por name que contenha a palavra gamer
 SELECT * FROM products
 WHERE name LIKE '%gamer%';
 
+--busca que termina em -----a na coluna name
+SELECT * FROM products
+WHERE name LIKE '%a'
+
+--busca que inicia em axxxxxxx, na coluna name
+SELECT * FROM products
+WHERE name LIKE 'a%'
+
 --Criar um novo usuario.id
 INSERT INTO users (id, name, email, password, create_at)
 VALUES
-    ('insira o id','insira o name', 'insira o email', 'insira o password', 'datatime(now)');
+    ('u004','Guilherme', 'guilherme@email.com', 'guilherme1234', 'datatime(now)');
 
 --Criar um novo produto
 INSERT INTO products (id, name,price, description, image_url)
@@ -64,14 +114,6 @@ UPDATE products
 SET description = 'Melhor da categoria'
 WHERE id = 'prod003';
 
---Editar update pelo id, ira alterar o set do usuario
-UPDATE users
-SET email = 'orcisantos@email.com'
-WHERE id ='u001'
-
---
-UPDATE products
-SET price = 500;
 
 select 
     id,
@@ -79,8 +121,22 @@ select
     price
 FROM products;
 
+--Editar update pelo id, ira alterar o set do usuario,
+--Pode ser uma ou varias colunas
+
+UPDATE users
+SET email = 'orcisantos@email.com'
+WHERE id ='u001';
+
 UPDATE products
 SET name = 'new name',
     price = 500,
     description = 'new description'
 WHERE id = 'prod006';
+
+--CRUD 
+
+--CREATE   criação        creat e insert
+--READ     leitura        select
+--UPDATE   atualização    update
+--DELETE   deleção        delete
