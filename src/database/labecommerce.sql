ccaf9c2f52b4f15579322d634ffab4cfb91b85b7
Orcisantos76 <orcisantos76@gmail.com>
2023-07-20 18:27:24 -0300

    Entrega do projeto finalizado

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -7,21 +7,66 @@ CREATE TABLE users (
     create_at TEXT NOT NULL
 );
 
+INSERT INTO users (id, name, email, password, create_at as createdAt)
+VALUES
+    
+    ('u001','Orci','orci@email.com','orci1234',datetime('now')),
+    ('u002','Ana','ana@email.com','ana1234',datetime('now')),
+    ('u003','Arthur','arthur@email.com','arthur1234',datetime('now'))
+    ('u004','Guilherme', 'guilherme@email.com', 'guilherme1234', 'datatime(now)');
+
+PRAGMA table_info (users);
+SELECT * FROM users;
+DROP TABLE users;
+
+
+
+
 CREATE TABLE products (
 	id TEXT PRIMARY KEY UNIQUE NOT NULL,
 	name TEXT NOT NULL,
 	price REAL NOT NULL,
 	description TEXT NOT NULL,
-    image_url TEXT NOT NULL
+    imageUrl TEXT NOT NULL
 );
+
+INSERT INTO products (id, name,price, description, imageUrl)
+VALUES
+    ('prod003', 'Teclado mecanico', 400.00 , 'Teclado mais preciso', 'https://picsum.photos/seed/teclado/400'  ),
+    ('prod004', 'Notebook', 6000.00 , 'Nitro 5', 'https://picsum.photos/seed/notebook/400'  ),
+    ('prod005', 'Cadeira gamer', 1500.00,'Cadeira gamer para horas de games com maior conforto', 'https://picsum.photos/seed/cadeiragamer/400');
+
+PRAGMA table_info (products);
+SELECT * FROM products;
+DROP TABLE products;
+
+
+
 
 CREATE TABLE purchases(
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     buyer TEXT NOT NULL,
     total_price REAL NOT NULL,
-    created_at TEXT NOT NULL,
-    Foreign Key (buyer) REFERENCES users(id)        
+    created_at TEXT DEFAULT(datetime('now'))  NOT NULL,
+    Foreign Key (buyer) REFERENCES users(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE        
 );
+
+INSERT INTO purchases (id,buyer,total_price,created_at)
+VALUES
+    ('p001','u001',400,datetime('now')),
+    ('p002','u001',300,datetime('now')),
+    ('p003','u002',350,datetime('now')),
+    ('p004','u002',500,datetime('now'));
+
+
+PRAGMA table_info (purchases);
+SELECT * FROM purchases;
+DROP TABLE purchases;
+
+
+
 
 CREATE TABLE purchases_products(
     purchase_id TEXT NOT NULL,
@@ -34,32 +79,19 @@ CREATE TABLE purchases_products(
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
-DROP TABLE purchases_products;
-
-INSERT INTO users (id, name, email, password, create_at)
-VALUES
-    ('u001','Orci','orci@email.com','orci1234',datetime('now')),
-    ('u002','Ana','ana@email.com','ana1234',datetime('now')),
-    ('u003','Arthur','arthur@email.com','arthur1234',datetime('now'));
-
-INSERT INTO products (id, name,price, description, image_url)
-VALUES
-    ('prod003', 'Teclado mecanico', 400.00 , 'Teclado mais preciso', 'https://picsum.photos/seed/teclado/400'  ),
-    ('prod004', 'Notebook', 6000.00 , 'Nitro 5', 'https://picsum.photos/seed/notebook/400'  ),
-    ('prod005', 'Cadeira gamer', 1500.00,'Cadeira gamer para horas de games com maior conforto', 'https://picsum.photos/seed/cadeiragamer/400');
-
-INSERT INTO purchases (id,buyer,total_price,created_at)
-VALUES
-    ('p001','u001',400,datetime('now')),
-    ('p002','u001',300,datetime('now')),
-    ('p003','u002',350,datetime('now')),
-    ('p004','u002',500,datetime('now'));
 
 INSERT INTO purchases_products (purchase_id, product_id, quantity)
 VALUES
     ('p004','prod003',2),
     ('p003','prod003',3),
     ('p004','prod003',2);
+
+PRAGMA table_info (purchases_products);
+SELECT * FROM purchases_products;
+DROP TABLE purchases_products;
+
+
+
 
 --atualizar valor.
 UPDATE purchases
@@ -73,26 +105,7 @@ INNER join products
 on products.id = purchases_products.product_id;
 
 
---Informaçao da tabela
-PRAGMA table_info ('users');
-
-PRAGMA table_info ('products');
-
-PRAGMA table_info ('purchases');
-
--- Excluir/apagar tabela
-DROP TABLE products;
-
---busca mostrar tabela de users:
-SELECT * FROM users;
-
---busca a tabela de products: 
-SELECT * FROM products;
-
-SELECT * FROM purchases;
-
-SELECT * FROM purchases_products;
-
+--mostrando so as colunas secionadas
 SELECT
     purchases.id AS idPurchase,
     users.id AS idUser,
@@ -104,22 +117,25 @@ FROM purchases
 INNER JOIN users
 ON purchases.buyer = users.id;
 
+--mostrando todas as colunas
+SELECT * FROM purchases
+INNER JOIN users
+ON purchases.buyer = users.id;
+
 --busca por name que contenha a palavra gamer
 SELECT * FROM products
 WHERE name LIKE '%gamer%';
 
 --busca que termina em -----a na coluna name
 SELECT * FROM products
-WHERE name LIKE '%a'
+WHERE name LIKE '%a';
 
 --busca que inicia em axxxxxxx, na coluna name
 SELECT * FROM products
-WHERE name LIKE 'a%'
+WHERE name LIKE 'a%';
 
 --Criar um novo usuario.id
-INSERT INTO users (id, name, email, password, create_at)
-VALUES
-    ('u004','Guilherme', 'guilherme@email.com', 'guilherme1234', 'datatime(now)');
+
 
 --Criar um novo produto
 INSERT INTO products (id, name,price, description, image_url)
@@ -143,9 +159,9 @@ WHERE id = 'prod003';
 
 
 select 
-    id,
-    name,
-    price
+    id AS idProduto,
+    name AS nomeProduto,
+    price As precoProduto
 FROM products;
 
 --Editar update pelo id, ira alterar o set do usuario,
